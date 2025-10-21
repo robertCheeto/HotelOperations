a9d1571e999d2820de8605d3d38581d105450489
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-21 11:32:30 -0400

    * added check for OT Hours

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,7 +8,7 @@ public class Main {
         double userNum;
 
         // testing the Employee Class with an Object called Chris
-        Employee Chris = new Employee(1, "Chris Brown", "Management",  25.46, 60);
+        Employee Chris = new Employee(1, "Chris Brown", "Management",  17, 50);
 
         //System.out.println("The total amount Chris earned is $" + Chris.getTotalWage());
         System.out.print("Enter time in: ");
@@ -20,15 +20,15 @@ public class Main {
         Chris.punchTimeCard(userNum, userNum2);
 
         System.out.println(Chris.getHoursWorked() + " <- hours worked");
+        System.out.println(Chris.getOvertimeHours() + "<- OT hours (if any)");
         System.out.println(Chris.getTotalWage() + " <- total wage");
-
 
         // testing the Room class
         Room room1 = new Room(2, 124.00, true, false);
-        System.out.println("Chris manages a hotel and has a room.");
+        System.out.println("\nChris manages a hotel and has a room.");
         System.out.println("The room has " + room1.getNumBeds() + " and it costs $" + room1.getPrice());
         System.out.println("Is the room Vacant? " + room1.isVacant() + ". Ok but is it available? " + room1.isAvailable());
-        System.out.println("testing if the room is checked in: " + room1.isCheckedIn());
+        System.out.println("\ntesting if the room is checked in: " + room1.isCheckedIn());
         System.out.println("testing if the room is checked out: " + room1.isCheckedOut());
         System.out.println("testing if the room is clean: " + room1.isCleanRoom());
 
