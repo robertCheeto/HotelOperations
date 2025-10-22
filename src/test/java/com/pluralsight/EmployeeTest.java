cc686658f008b22316215953bde6da19a62ffbcc
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-22 15:38:46 -0400

    * conducted unit testing for punchIn() and punchOut()

---
+++ src/test/java/com/pluralsight/EmployeeTest.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeTest {
+
+    @Test
+    void punchIn_time_should_be_current_time() {
+        Employee dave = new Employee(1,"dave", "accounting", 12);
+
+        dave.punchIn();
+
+        assertEquals(0, dave.getHoursWorked());
+        assertEquals(0, dave.getOvertimeHours());
+        assertEquals(0, dave.getTotalWage());
+
+    }
+
+    @Test
+    void total_hours_worked_time_should_be_zero_after_immediately_punchingIn() {
+        Employee dave = new Employee(1,"dave", "accounting", 12);
+
+        dave.punchIn();
+        dave.punchOut();
+
+        assertEquals(0, dave.getHoursWorked());
+        assertEquals(0, dave.getOvertimeHours());
+        assertEquals(0, dave.getTotalWage());
+
+    }
+
+}
