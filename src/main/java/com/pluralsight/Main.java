69bb9a00c3257f006e79c5e006126d21f5eee55a
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-22 10:54:56 -0400

    * began testing the bookRoom() and Hotel object in Main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -18,12 +18,12 @@ public class Main {
 //        keyboard.nextLine();
 //        Chris.punchTimeCard(userNum, userNum2);
 
-        Chris.punchIn();
-        Chris.punchOut();
-
-        System.out.println(Chris.getHoursWorked() + " <- hours worked");
-        System.out.println(Chris.getOvertimeHours() + "<- OT hours (if any)");
-        System.out.println(Chris.getTotalWage() + " <- total wage");
+//        Chris.punchIn();
+//        Chris.punchOut();
+//
+//        System.out.println(Chris.getHoursWorked() + " <- hours worked");
+//        System.out.println(Chris.getOvertimeHours() + "<- OT hours (if any)");
+//        System.out.println(Chris.getTotalWage() + " <- total wage");
 
 //        // testing the Room class
 //        Room room1 = new Room(2, 124.00, true, false);
@@ -34,6 +34,20 @@ public class Main {
 //        System.out.println("\ntesting if the room is checked in: " + room1.isCheckedIn());
 //        System.out.println("testing if the room is checked out: " + room1.isCheckedOut());
 //        System.out.println("testing if the room is dirty: " + room1.isDirtyRoom());
+
+        // testing the Hotel class
+//            public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+//            this.name = name;
+//            this.numberOfSuites = numberOfSuites;
+//            this.numberOfRooms = numberOfRooms;
+//            this.bookedSuites = bookedSuites;
+//            this.bookedBasicRooms = bookedBasicRooms;
+        Hotel BestWestern = new Hotel("BW1", 10,20,3,5);
+//        BestWestern.bookRoom(5,false);
+        System.out.println(BestWestern.bookRoom(5,false));
+        System.out.println("The hotel now has the following rooms remaining: " + BestWestern.getBookedBasicRooms());
+        System.out.println("The hotel now has the following suites remaining: " + BestWestern.getBookedSuites());
+        System.out.println("The hotel has this many rooms total: " + BestWestern.getNumberOfRooms());
 
     }
 }
